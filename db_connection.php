cbdae7b9c106fe85145c1842b5ff82dabf86ddd4
AbdelrahmanAbdo265 <abdelrahmanabdullah265@yahoo.com>
2024-12-23 11:29:09 +0200

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -2,7 +2,7 @@
     $servername = "localhost"; // Localhost is the default for local servers
     $username = "root";        // Default username for XAMPP and WAMP is 'root'
     $password = "";            // Default password for XAMPP and WAMP is empty
-    $db_name = "shopping";      // Match this to the database name you created    
+    $db_name = "shopping_database";      // Match this to the database name you created    
     $conn = new mysqli($servername, $username, $password, $db_name, 3306);
     if($conn->connect_error){
         die("Connection failed".$conn->connect_error);
