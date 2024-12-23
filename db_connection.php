3b5b2904ab1bd9c9def860bb3d3c01f7ba706f30
AbdelrahmanAbdullah265 <abdelrahmanabdullah265@yahoo.com>
2024-12-23 11:20:50 +0200

    first commit

---
+++ db_connection.php
@@ -0,0 +1,11 @@
+<?php
+    $servername = "localhost"; // Localhost is the default for local servers
+    $username = "root";        // Default username for XAMPP and WAMP is 'root'
+    $password = "";            // Default password for XAMPP and WAMP is empty
+    $db_name = "shopping";      // Match this to the database name you created    
+    $conn = new mysqli($servername, $username, $password, $db_name, 3306);
+    if($conn->connect_error){
+        die("Connection failed".$conn->connect_error);
+    }
+    echo "";
+?>
